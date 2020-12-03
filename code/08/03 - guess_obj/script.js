0855890d7bd8acee75775b41205e9015e187533a
Mikael Olsson <mikael.olsson@emmio.se>
2020-12-03 15:29:24 +0100

    Guess number

--- code/08/03 - guess_obj/script.js
+++ code/08/03 - guess_obj/script.js
@@ -1,33 +1,48 @@
-function random_number(number = 100) {
-    return Math.round(Math.random() * number);
-}
+class GuessNumber {
+    constructor() {
+        this.random = null;
+        this.continue_game = true;
+        this.counter = null;
+        this.message = null;
 
-function guess_number() {
-    let random = random_number();
-    let continue_game = true;
-    let counter = 0;
-    let message = "";
+        this.init();
+        this.start_game();
+    }
 
-    while(continue_game) {
-        let answer = prompt(message + "\nJag tänker på ett nummer 1-100. Vilket tror du det är? (Avsluta med Avbryt.)");
-        let guess = Number(answer);
-        counter++;
+    static random_number(number = 100) {
+        return Math.round(Math.random() * number);
+    }
 
-        if (!guess) {
-            continue_game = false;
-            break;
-        }
+    init() {
+        this.random = GuessNumber.random_number();
+        this.counter = 0;
+        this.message = "";
+    }
 
-        if (guess < random) {
-            message = "Mitt tal är större.";
-        } else if (guess > random) {
-            message = "Mitt tal är mindre.";
-        } else {
-            continue_game = confirm(`Du gissade rätt! det tog ${counter} gissningar! Spela igen?`);
-            counter = 0;
-            message = "";
-            random = random_number();
+    start_game() {
+        while(this.continue_game) {
+            let answer = prompt(this.message + "\nJag tänker på ett nummer 1-100. Vilket tror du det är? (Avsluta med Avbryt.)");
+            let guess = Number(answer);
+            this.counter++;
+    
+            if (!guess) {
+                this.continue_game = false;
+                //break;
+            }
+    
+            if (guess < this.random) {
+                this.message = "Mitt tal är större.";
+            } else if (guess > this.random) {
+                this.message = "Mitt tal är mindre.";
+            } else {
+                this.continue_game = confirm(`Du gissade rätt! det tog ${this.counter} gissningar! Spela igen?`);
+                this.init();
+            }
         }
     }
 }
 
+//let game = new GuessNumber();
+function start_game() {
+    new GuessNumber();
+}
