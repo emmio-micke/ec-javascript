ecdc37a3100ca0435950bde7c50b2795e855537f
Mikael Olsson <mikael.olsson@emmio.se>
2020-12-07 09:40:42 +0100

    Added some solutions, a lecture and an example

---
+++ code/exercises/26 - combine_arrays.js
@@ -0,0 +1,8 @@
+/*
+
+Skriv en funktion som tar emot två arrayer som parametrar och lägg till den ena arrayens innehåll i den andra arrayen och returnerar den med hjälp av spread-operatorn.
+
+Tips: https://davidwalsh.name/spread-operator
+
+*/
+
---
+++ code/exercises/30 - iterate_object.js
@@ -0,0 +1,16 @@
+/*
+
+Skriv en funktion som tar emot ett objekt och skriver ut dess egenskaper. Objektet kan vara vilket som helst, men här är ett exempel:
+
+let student = {
+  name : "Jessica",
+  age : 44,
+  grade : "VG"
+};
+
+Utskrift: name,age,grade
+Ni kan ha nytta av denna dokumentation:
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+
+*/
